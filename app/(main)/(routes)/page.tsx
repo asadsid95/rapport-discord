b6942c37c0e2d61a7762cc8cd118d5b801765aef
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-26 17:58:15 -0400

    authentication added

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div className='text-3xl font-bold text-indigo-500'>
-      Protected route
+      <UserButton afterSignOutUrl='' />
     </div>
   )
 }
