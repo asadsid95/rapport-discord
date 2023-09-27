23eed2412d4b4a570d75260e52347a65a0113b5b
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2023-09-27 13:26:53 -0400

    API created for server creation

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -9,6 +9,8 @@ import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import { useEffect, useState } from 'react'
 import { FileUpload } from '../file-upload'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -21,6 +23,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
 
+    const router = useRouter()
     const [isMounted, setIsMounted] = useState(false)
 
     useEffect(() => {
@@ -38,7 +41,15 @@ export const InitialModal = () => {
     const isLoading = form.formState.isSubmitting
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values)
+        try {
+            await axios.post('/api/servers', values)
+            form.reset()
+
+            router.refresh()
+            window.location.reload()
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     if (!isMounted) {
